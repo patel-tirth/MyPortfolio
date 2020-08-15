a6a9b6863712af771088a863015f8e73c8bf72e8
patel-tirth <pateltirth1750@gmail.com>
2020-08-15 00:15:01 -0400

    update code

--- dist/js/script.js
+++ dist/js/script.js
@@ -114,6 +114,14 @@ $('.owl-carousel').owlCarousel({
 			return false;
 		});
 
+		$("#learnMore p a").click(function(e){
+			e.preventDefault();
+
+			var targetElement = $(this).attr("href");
+			var targetPosition = $(targetElement).offset().top;
+
+			$("html, body").animate({scrollTop: targetPosition - 60 }, "slow");
+		});
 		$("#navigation li a").click(function(e){
 			e.preventDefault();
 
